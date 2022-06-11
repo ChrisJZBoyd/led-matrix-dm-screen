a1d3398d4f2f7000150419cdcbac0677e8ed7d6c
SaintChristopher <christopherjzboyd@gmail.com>
2022-06-11 16:58:39 +0100

    Debugging the madlib generator

--- Test.py
+++ Test.py
@@ -6,13 +6,13 @@ loop = 1
 while (loop < 10):
 #All questions that the program asks the user
     noun = input ("Choose a noun: ")
-    p_noun = input("Choose a prural noun: ")
+    p_noun = input("Choose a plural noun: ")
     noun2 = input("Choose a noun: ")
     place = input("Choose a place: ")
     adjective = input("Choose an adjective: ")
     noun3 = input("Choose a noun: ")
 #Displays the story based on the users input
-    print ("-----------------------------")
+    print ("------------------------------")
     print ("Be kind to your",noun,"- footed", p_noun)
     print ("For a duck may be somebody's", noun2,",")
     print ("Be kind to your", p_noun,"in",place)
